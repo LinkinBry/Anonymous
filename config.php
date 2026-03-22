fb89aeed05e9879b4f5ac8bd1d063ede0ab9ac6a
LinkinBry <bryanjaylicuanandomingo@gmail.com>
2026-03-22 02:49:51 +0000

    sad

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-$host = "127.0.0.1"; // use IP instead of "localhost" to force TCP
+$host = "127.0.0.1";
 $user = "root";
-$password = "";
+$password = "root";  // now has a password
 $dbname = "faculty_review_system";
 
 $conn = mysqli_connect($host, $user, $password, $dbname);

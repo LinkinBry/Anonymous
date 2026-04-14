e1f551a2f6838790f182eae1816f9e77b060b1c0
LinkinBry <bryanjaylicuanandomingo@gmail.com>
2026-04-14 18:19:07 +0800

    Delete mark_notifications_read.php

--- mark_notifications_read.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "config.php";
-if (!isset($_SESSION['user_id'])) { http_response_code(401); exit(); }
-$user_id = $_SESSION['user_id'];
-mysqli_query($conn, "UPDATE notifications SET status='read' WHERE user_id='$user_id' AND status='unread'");
-echo json_encode(['success' => true]);
-?>
